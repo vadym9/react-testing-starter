cd18259c471ac4884721c252d3c3593b7f92626c
Vadym Putrov <vadym.putrov@chisw.com>
2020-04-06 13:16:47 +0300

    destructure Episode

--- src/components/Episode/Episode.tsx
+++ src/components/Episode/Episode.tsx
@@ -4,20 +4,23 @@ import { v1 as uuidv1 } from 'uuid';
 import { Episode } from '../../global-models';
 import { EpisodeAllProps } from './models/episode-types';
 
-const Episode = ({ episode }: EpisodeAllProps): JSX.Element => (
-  <li key={uuidv1()} className="episode flex jcsb ai-center">
-    <div>
-      <h3>{episode.title}</h3>
-    </div>
-    <div className="btn-block">
-      <a className="btn-link" href={episode.video_url}>
-        Watch video
-      </a>
-      <a className="btn-link" href={episode.forum_url}>
-        Open forum
-      </a>
-    </div>
-  </li>
-);
+const Episode = ({ episode }: EpisodeAllProps): JSX.Element => {
+  const { title, video_url, forum_url } = episode;
+  return (
+    <li key={uuidv1()} className="episode flex jcsb ai-center">
+      <div>
+        <h3>{title}</h3>
+      </div>
+      <div className="btn-block">
+        <a className="btn-link" href={video_url}>
+          Watch video
+        </a>
+        <a className="btn-link" href={forum_url}>
+          Open forum
+        </a>
+      </div>
+    </li>
+  );
+};
 
 export default Episode;
